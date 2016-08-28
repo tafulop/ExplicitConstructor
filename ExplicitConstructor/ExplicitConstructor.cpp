a78425d5a2a7862bdc31cb94e6a794e4def797f0
tafulop <tamas.flp@gmail.com>
2016-08-28 17:00:18 +0200

    Demo code completed.

--- ExplicitConstructor/ExplicitConstructor.cpp
+++ ExplicitConstructor/ExplicitConstructor.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
 
+using namespace std;
+
+class Foo {
+
+private:
+	int m_i;
+
+public:
+	 explicit Foo(int i) {
+		m_i = i;
+	}
+};
+
+void fn(Foo x) {}
 
 int main()
 {
+	// would be fine without "explicit" keyword
+	// fn(1); 
+
+	// more approriate, no implicit conversion
+	fn(Foo(1));
+
     return 0;
 }
 
